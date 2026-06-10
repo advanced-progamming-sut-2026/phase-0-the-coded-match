f07b4f004605e714ddefd4352074e89462e89f72
Fatemeh <fatemehmehrara2007@gmail.com>
2026-06-10 18:20:12 +0330

    new changes

--- src/Model/ZombieArmor.java
+++ src/Model/ZombieArmor.java
@@ -1,17 +1,36 @@
 package Model;
 
-import Enums.ArmorType;
-
 public class ZombieArmor {
 
-    private ArmorType type;
-    private int hp;
+    private ZombieArmorData data;
+    private int currentHp;
 
-    public void takeDamage (int damage){
-         hp -= damage;
+    public ZombieArmor(ZombieArmorData data) {
+        this.data = data;
+        this.currentHp = data.getHp();
     }
 
-    public boolean isDestroyed () {
-        return hp <= 0;
+    public int takeDamage(int damage) {
+        currentHp -= damage;
+
+        if (currentHp < 0) {
+            int remainingDamage = -currentHp;
+            currentHp = 0;
+            return remainingDamage;
+        }
+
+        return 0;
+    }
+
+    public boolean isDestroyed() {
+        return currentHp <= 0;
+    }
+
+    public ZombieArmorData getData() {
+        return data;
+    }
+
+    public int getCurrentHp() {
+        return currentHp;
     }
 }
