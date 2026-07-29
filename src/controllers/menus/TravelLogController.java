b8c2440b96c0b0fe531e7f05947175e0c361e216
Soha Hosseini <ssohahosseinik@gmail.com>
2026-07-29 18:57:16 +0330

    debug

--- src/controllers/menus/TravelLogController.java
+++ src/controllers/menus/TravelLogController.java
@@ -3,7 +3,9 @@ package controllers.menus;
 import enums.Commands;
 import enums.Menu;
 import models.App;
+import models.Quest;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,5 +29,23 @@ public class TravelLogController {
             default:
                 return "invalid page name";
         }
+    }
+    public static StringBuilder showQuests() {
+        StringBuilder sb = new StringBuilder();
+        List<Quest> quests = App.getCurrentUser().getQuestsModel().getAvailableQuests();
+        sb.append("--- Quests ---").append("\n");
+        for (Quest quest : quests) {
+            sb.append("name: ").append(quest.getQuestName()).append("\n");
+            sb.append("condition: ").append(quest.getQuestData().getConditionText()).append("\n");
+            sb.append("progression: ").append(quest.getCurrentValue()).append("\n");
+            sb.append("---------------------------");
+        }
+        return sb;
+    }
+    public static StringBuilder showMinigames() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("--- MiniGames ---").append("\n");
+        sb.append("Vasebreaker - Wallnut Bowling - I, Zombie - Beghouled - Zombotany");
+        return sb;
     }
 }
--- src/views/TravelLogView.java
+++ src/views/TravelLogView.java
@@ -7,9 +7,12 @@ public class TravelLogView {
     public static void check(String input) {
         if (input.matches(Commands.TRAVEL_LOG_PAGE.getPattern())) {
             System.out.println(TravelLogController.changePage(input));
+        } else if (input.matches("^\\s*show\\s+quests\\s*$")) {
+            System.out.println(TravelLogController.showQuests().toString());
+        } else if (input.matches("^\\s*show\\s+minigames\\s*$")) {
+            System.out.println(TravelLogController.showMinigames().toString());
         } else {
             System.out.println("invalid command");
         }
     }
-
 }
